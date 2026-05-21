aabd0ba13a2acf827038159fcd5d40d6fb9a6d63
jozefajogwu <jozefajogwu@gmail.com>
2026-05-21 08:01:28 +0100

    changes

--- rgms_core/settings.py
+++ rgms_core/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'rgms_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#        conn_max_age=600
+#    )
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
